b508740d24910dd28bdd152647a555ccd7d2ea06
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2020-07-15 11:52:35 +0200

    Display results as a dataframe (props and molar fraction)

--- Solver/Functions/Display/displayResults.py
+++ Solver/Functions/Display/displayResults.py
@@ -24,7 +24,7 @@ def displayResults(self, *args):
         str2 = args[2]
     print('-----------------------------------------------------------')
     print('PROBLEM TYPE: %s  |  EQUIVALENCE RATIO = %4.3f\n' % (ProblemType, strR.phi))
-    
+    # Print properties
     props = ['T', 'p', 'r', 'h', 'e',
               's', 'cp', 'gamma']
     units = ['K', 'bar', 'kg/m3', 'kJ/kg', 'kJ/kg',
@@ -32,18 +32,38 @@ def displayResults(self, *args):
     hier_index = list(zip(props, units))
     hier_index = pd.MultiIndex.from_tuples(hier_index)
     data = np.array([
-        [strR.T],
-        [strR.p],
-        [strR.rho],
-        [strR.h/strR.mi],
-        [strR.e/strR.mi],
-        [strR.S],
-        [strR.cP/strR.mi*1e-3],
-        [strR.cP/strR.cV]
+        [strR.T, strR.T],
+        [strR.p, strR.p],
+        [strR.rho, strR.rho],
+        [strR.h/strR.mi, strR.h/strR.mi],
+        [strR.e/strR.mi, strR.e/strR.mi],
+        [strR.S, strR.S],
+        [strR.cP/strR.mi *1e-3, strR.cP/strR.mi *1e-3] ,
+        [strR.cP/strR.cV, strR.cP/strR.cV]
         ])
-    df = pd.DataFrame(data, hier_index, ['REACTANTS'])
+    df = pd.DataFrame(data, hier_index, ['REACTANTS', 'PRODUCTS'])
     df = df.round(3)
     df.index.names = ['PROPERTIES', 'UNITS']
-    print(df)
-    df = pd.DataFrame(strR.Xi, index=NameSpecies, columns=['Xi'])
+    print(df, '\n')
+    # Print reactants
+    pd.options.display.float_format = '{:.4E}'.format
+    pd.set_option('display.max_rows', None)
+    
+    df = pd.DataFrame(strR.Xi, index=NameSpecies, columns=['Xi [-]'])
+
+    df.sort_values('Xi [-]', ascending=False, inplace=True)
+    df.index.names = ['REACTANTS']
+    print(df[df['Xi [-]']>0])
+    
+    df2 = pd.DataFrame([strR.Xi.sum()], index=['TOTAL    '], columns=[''])
+    print(df2, '\n')
+    # Print products
+    df = pd.DataFrame(strR.Xi, index=NameSpecies, columns=['Xi [-]'])
+    df.sort_values('Xi [-]', ascending=False, inplace=True)
+    df.index.names = ['PRODUCTS ']
+    print(df[df['Xi [-]']>mintol_display])
+    
+    df2 = pd.DataFrame([strR.Xi.sum()], index=['TOTAL    '], columns=[''])
+    print(df2, '\n')
+    
     
