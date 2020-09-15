acf086336d57036e6f2564adea06a416a242f324
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2020-09-15 18:25:01 +0200

    Fixed reactants not contained in compute list

--- Settings/Define_FOI.py
+++ Settings/Define_FOI.py
@@ -8,13 +8,40 @@ Created on Tue Jun 30 16:28:00 2020
          PhD Candidate - Group Fluid Mechanics
          Office 1.1.D17, Universidad Carlos III de Madrid
 """
+import numpy as np
 from Solver.Functions.SetSpecies import SetSpecies
 from Solver.Functions.ComputeProperties import ComputeProperties
+from NASA_database.set_element_matrix import set_element_matrix
+
+def add_species(self, Species):
+    for species in Species:
+            if species not in self.S.List_Compute_Species:
+                # Add species and index in the compute and index list, respectively
+                self.S.List_Compute_Species.append(species) 
+                self.S.ind_all.append(self.S.N_Compute_Species)
+                # Add one row to A0 and M0 matrix, respectively
+                self.C.A0.Value = np.concatenate((self.C.A0.Value, np.zeros((1, self.E.NE))))
+                self.C.M0.Value = np.concatenate((self.C.M0.Value, np.zeros((1, 12))))
+                self.C.N0.Value = self.C.M0.Value[:, [0, 9]] 
+                # Obtain the formula of the species
+                txFormula = self.strThProp[species].txFormula 
+                # Obtain the element matrix
+                self.strThProp[species].Element_matrix = set_element_matrix(
+                    txFormula, self.E.ElementsUpper) 
+                # Obtain index of the elements that compound the species and the number of atoms of each element
+                ind_Elements, atoms = (self.strThProp[species].Element_matrix[0, :], self.strThProp[species].Element_matrix[1, :])
+                # Update A0 matrix
+                for ind_Element, atom in zip(ind_Elements, atoms):
+                    self.C.A0.Value[self.S.N_Compute_Species, int(ind_Element)] = atom
+                # Increase one the number of species 
+                self.S.N_Compute_Species += 1
+    return (self.S.List_Compute_Species, self.C.M0.Value)
 
 
 def Define_F(self, Species=None):
     if Species:  # not empty, i.e., there is/are fuel/s in the mixture
         self.PD.S_Fuel, self.PD.N_Fuel = unpack_dict(Species)
+        self.S.List_Compute_Species, self.C.M0.Value = add_species(self, self.PD.S_Fuel)
         self.PD.R_Fuel = SetSpecies(
             self, self.PD.S_Fuel, self.PD.N_Fuel, self.PD.TR.Value)
         self.PS.strR_Fuel = ComputeProperties(
@@ -29,6 +56,7 @@ def Define_F(self, Species=None):
 
 def Define_O(self, Species=None):
     self.PD.S_Oxidizer, self.PD.N_Oxidizer = unpack_dict(Species)
+    self.S.List_Compute_Species, self.C.M0.Value = add_species(self, self.PD.S_Oxidizer)
     if Species:  # not empty, i.e., there is/are oxidizer/s in the mixture
         self.PD.R_Oxidizer = SetSpecies(
             self, self.PD.S_Oxidizer, self.PD.N_Oxidizer, self.PD.TR.Value)
@@ -38,6 +66,7 @@ def Define_O(self, Species=None):
 def Define_I(self, Species=None):
     if Species:  # not empty, i.e., there is/are oxidizer/s in the mixture
         self.PD.S_Inert, self.PD.N_Inert = unpack_dict(Species)
+        self.S.List_Compute_Species, self.C.M0.Value = add_species(self, self.PD.S_Inert)
         self.PD.R_Inert = SetSpecies(
             self, self.PD.S_Inert, self.PD.N_Inert, self.PD.TR.Value)
     else:
