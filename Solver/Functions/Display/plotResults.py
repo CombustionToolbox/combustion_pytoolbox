da29084dd1eb29e804005a885512ba98110348b2
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2020-10-05 11:05:49 +0200

    Plot molar fraction vs equivalence ratio

---
+++ Solver/Functions/Display/plotResults.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import (PchipInterpolator as pchip)
+
+def plotMoles(self, x_vec, display_species, mintol, xlabel, ax=None, plt_kwargs={}, **kwargs):
+    if not ax:
+        ax = plt.subplot()
+    # Data
+    struct = self.PS.strP
+    Nx = len(x_vec)
+    Ndisplay = len(display_species)
+    y_matrix = np.zeros((Ndisplay, Nx))
+    Ndisplay = set()
+    # Display tolerance requirements
+    for i in range(Nx):
+        y_matrix[:, i] = struct[i].Xi
+        for ind in range(self.S.NS):
+            if struct[i].Xi[ind] > mintol:
+                Ndisplay.add(ind)
+    # Plot configuration
+    plt.set_cmap('Spectral')
+    ax.set_xlim(x_vec.min(), x_vec.max())
+    ax.set_ylim(mintol, 1.0)
+    ax.set_yscale('log')
+    labels = display_species
+    # Plot
+    for j in Ndisplay:
+        ax.plot(x_vec,y_matrix[j, :], '-d', **plt_kwargs, **kwargs)
+    # Legend    
+    ax.legend(labels=labels, loc='right')
+    plt.show()
+    return self
+def plotResults(self, display_species=None, mintol=1e-16):
+    phi = self.PD.phi.Value
+    ProblemType = self.PD.ProblemType
+    # Figure configuration
+    plot_params = {'linewidth': 2}
+    if len(phi) > 1 and all(phi[1::] != phi[0]) and ProblemType != 'DET_OVERDRIVEN':
+        plotMoles(self, phi, display_species, mintol, xlabel='Equivalence ratio', plt_kwargs = plot_params)
+    
