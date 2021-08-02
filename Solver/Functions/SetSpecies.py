8d215b80184ef8fac5ff2c312a49815f930b29dc
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-08-02 16:33:27 +0200

    rewritten species_xx functions

--- Solver/Functions/SetSpecies.py
+++ Solver/Functions/SetSpecies.py
@@ -9,6 +9,9 @@ Created on Tue Jun 30 16:28:00 2020
          Office 1.1.D17, Universidad Carlos III de Madrid
 """
 import numpy as np
+from NASA_database.set_reference_form_of_elements_with_T_intervals import set_reference_form_of_elements_with_T_intervals
+from NASA_database.isRefElm import isRefElm
+from NASA_database.detect_location_of_phase_specifier import detect_location_of_phase_specifier
 
 def SetSpecies(self, Species, N, T):
     M = self.C.M0.Value.copy()
@@ -18,11 +21,12 @@ def SetSpecies(self, Species, N, T):
         hfi = self.strThProp[species].hf / 1000.
         efi = self.strThProp[species].ef / 1000.
         if len(self.strThProp[species].T > 1):
-            cPi = species_cP(species, T, self.strThProp)
-            cVi = species_cV(species, T, self.strThProp)
-            DeTi = species_DeT(species, T, self.strThProp)
-            DhTi = species_DhT(species, T, self.strThProp)
-            s0i = species_s0(species, T, self.strThProp)
+            tInterval = get_tInterval(species, T, self.strThProp)
+            cPi = species_cP(species, T, self.strThProp, tInterval, R0)
+            cVi = species_cV(species, T, self.strThProp, tInterval, R0)
+            DeTi = species_DeT(species, T, self.strThProp, tInterval, R0)
+            DhTi = species_DhT(species, T, self.strThProp, tInterval, R0)
+            s0i = species_s0(species, T, self.strThProp, tInterval, R0)
             swtCondensed = self.strThProp[species].swtCondensed
             mi = n * self.strThProp[species].mm
             mmi = self.strThProp[species].mm
@@ -45,29 +49,51 @@ def SetSpecies(self, Species, N, T):
             (n, n * np.array([hfi, DhTi, efi, DeTi, cPi, cVi, s0i]), pVi, swtCondensed, mi, mmi), axis=None)
     return M
 
+def get_tInterval(species, T, strThProp):
+    # Select the appropriate temperature interval
+    for i in range(0, strThProp[species].ctTInt):
+        if (T >= strThProp[species].tRange[i][0]) and (T <= strThProp[species].tRange[i][1]):
+            tInterval = i
+    return tInterval
 
-def species_cP(species, T, strThProp):
-    return strThProp[species].cPcurve(T)  # [J/mol K]
-
-
-def species_cV(species, T, strThProp):
-    return strThProp[species].cVcurve(T)  # [J/mol K]
-
-
-def species_DeT(species, T, strThProp):
-    return strThProp[species].DeTcurve(T) / 1000.  # [kJ/mol]
+def species_cP(species, T, strThProp, tInterval, R0):
+    return R0 * sum(strThProp[species].a[tInterval] * T**np.array(strThProp[species].tExponents[tInterval]))  # [J/mol K]
 
 
-def species_DhT(species, T, strThProp):
-    return strThProp[species].DhTcurve(T) / 1000.  # [kJ/mol]
+def species_cV(species, T, strThProp, tInterval, R0):
+    cP = species_cP(species, T, strThProp, tInterval, R0)
+    return cP - R0 # [J/mol K]
 
 
-def species_g0(species, T, strThProp):
-    return strThProp[species].g0curve(T) / 1000.  # [kJ/mol]
+def species_DeT(species, T, strThProp, tInterval, R0):
+    Tref = 298.15  # [K]
+    H0 = species_DhT(species, T, strThProp, tInterval, R0) * 1000
+    E0 = strThProp[species].ef + (H0 - strThProp[species].hf) - (1 - strThProp[species].swtCondensed) * R0 * (T - Tref)
+    return (E0 - strThProp[species].ef) / 1000.  # [kJ/mol]
 
 
-def species_s0(species, T, strThProp):
-    return strThProp[species].s0curve(T) / 1000.  # [kJ/mol K]
+def species_DhT(species, T, strThProp, tInterval, R0):
+    aux = np.array([-1, np.log(T), 1, 1/2, 1/3, 1/4, 1/5, 0])
+    H0 = R0 * T * \
+            (sum(strThProp[species].a[tInterval] * T**np.array(strThProp[species].tExponents[tInterval])
+                 * aux) + strThProp[species].b[tInterval][0] / T)
+    return (H0 - strThProp[species].hf) / 1000.  # [kJ/mol]
+
+
+def species_g0(species, T, strThProp, tInterval, R0):
+    DhT = species_DhT(species, T, strThProp, tInterval, R0) # [kJ/mol]
+    H0 = DhT + strThProp[species].hf / 1000 # [kJ/mol]
+    S0 = species_s0(species, T, strThProp, tInterval, R0) # [kJ/mol]
+    return H0 - T * S0  # [kJ/mol]
+
+
+def species_s0(species, T, strThProp, tInterval, R0):
+    aux = np.array([-1/2, -1, np.log(T), 1, 1/2, 1/3, 1/4, 0])
+    S0 = R0 * \
+            (sum(strThProp[species].a[tInterval] * T**np.array(strThProp[species].tExponents[tInterval])
+                 * aux) + strThProp[species].b[tInterval][1])
+    return S0 / 1000.  # [kJ/mol K]
+
 
 def equil_constant(DG0, TP, R0):
     return np.exp(-DG0 / (R0 * TP))
