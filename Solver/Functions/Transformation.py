ef77e96d2831bd894310b392b1fba70eef92c2d5
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-08-05 15:22:07 +0200

    get and set attributes from ProblemDescription

--- Solver/Functions/Transformation.py
+++ Solver/Functions/Transformation.py
@@ -1,2 +1,5 @@
 def set_transformation(self, attr_name, attr_value):
     setattr(getattr(self.PD, attr_name), 'Value', attr_value)
+    
+def get_transformation(self, attr_name):
+    return getattr(getattr(self.PD, attr_name), 'Value')
